4729fd79b14d2aa0d6b5c5cda00f2262dce162dc
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2026-01-30 20:46:14 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "DLList.h"
+#include "DLList.cpp"
+
+int main() {
+    DLList<char> l;
+
+    l.pop_front();
+    l.pop_back();
+
+    l.push_back('C');
+    l.pop_front();
+    l.push_front('A');
+    l.pop_back();
+    l.push_back('W');
+    l.push_front('V');
+    l.push_back('6');
+    l.push_front('F');
+    l.push_front('W');
+
+    l.print_backwards();
+
+    return 0;
+}
